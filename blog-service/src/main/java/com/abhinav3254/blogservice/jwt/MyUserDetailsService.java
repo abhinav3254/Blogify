c6481325fbed361ffe6f20c0569d86a4d09d9b90
abhinav3254 <abhinav3254@gmail.com>
2024-05-19 23:52:27 +0530

    spring cloud

---
+++ blog-service/src/main/java/com/abhinav3254/blogservice/jwt/MyUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.abhinav3254.blogservice.jwt;
+
+
+
+
+import com.abhinav3254.authservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    com.abhinav3254.authservice.model.User user;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<com.abhinav3254.authservice.model.User> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            return new User(user.getUsername(), user.getPassword(), new ArrayList<>());
+        }
+
+        throw new UsernameNotFoundException("user not found by email/phone "+username);
+
+    }
+
+
+
+    public com.abhinav3254.authservice.model.User getUserDetails() {
+        return user;
+    }
+
+}
