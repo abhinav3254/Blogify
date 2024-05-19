25fe34bd78b319683bd55f625921dbad25b4cf14
abhinav3254 <abhinav3254@gmail.com>
2024-05-19 23:46:26 +0530

    removed .idead

---
+++ blog-service/src/test/java/com/abhinav3254/blogservice/BlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav3254.blogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
