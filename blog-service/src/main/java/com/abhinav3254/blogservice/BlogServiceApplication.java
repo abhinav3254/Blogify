9336b61fcdaedd3ab806c0b56517de1c9b7cfb14
abhinav3254 <abhinav3254@gmail.com>
2024-05-20 00:34:12 +0530

    version fix

--- blog-service/src/main/java/com/abhinav3254/blogservice/BlogServiceApplication.java
+++ blog-service/src/main/java/com/abhinav3254/blogservice/BlogServiceApplication.java
@@ -2,7 +2,10 @@ package com.abhinav3254.blogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class BlogServiceApplication {
 
